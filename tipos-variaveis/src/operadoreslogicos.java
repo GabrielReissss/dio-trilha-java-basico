10181c4e8c9cd8b3524bc182ddab98850aa08e36
Gabriel Reis <reisgabriel@me.com>
2022-11-22 09:15:15 -0300

    Atualização

---
+++ tipos-variaveis/src/operadoreslogicos.java
@@ -0,0 +1,17 @@
+public class operadoreslogicos {
+    
+    public static void main(String[] args) {  //padrão para todos tipos de operadores
+        int numero = 5;
+
+        numero = - numero;
+
+        System.out.println(-- numero); //system.out.println serve para mostrar o resultado em tela do que está sendo desenvolvido, assim como o console.log no Javascript
+        
+
+        
+        //usando operador lógico de negação para expressoes boleanas
+        boolean variavel = true;
+
+        System.out.println(!variavel);
+    }
+}
---
+++ tipos-variaveis/src/relacionais.java
@@ -0,0 +1,12 @@
+public class relacionais {
+    
+    public static void main(String[] args) {
+
+        int numero1 = 10;
+        int numero2 = 7;
+
+        boolean simNao = numero1 != numero2;
+
+        System.out.println("numeroUm e igual a numeroDois?" + simNao);
+        }
+}
